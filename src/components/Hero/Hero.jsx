48a4c6aa93afa9d913e1ea4665d45371f8b90e0e
AnnBalias <annbalias@gmail.com>
2025-06-09 23:07:52 +0300

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,9 +3,14 @@ import css from './Hero.module.css';
 
 export const Hero = () => {
   return (
-    <>
-      <p className={css.p}>Hero</p>
-      <Link to="/">Home</Link>
-    </>
+    <div className={css.hero}>
+      <h1 className={css.title}>Find your perfect rental car</h1>
+      <h2 className={css.subtitle}>
+        Reliable and budget-friendly rentals for any journey
+      </h2>
+      <Link to="/catalog" className={css.heroLink}>
+        View Catalog
+      </Link>
+    </div>
   );
 };
