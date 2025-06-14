45a3f498525ec90c8ce2b27c71b25cb3ac3ca7e1
AnnBalias <annbalias@gmail.com>
2025-06-14 12:31:31 +0300

    notfound-page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+import { IconContext } from 'react-icons';
+import { PiSmileySadThin } from 'react-icons/pi';
+import css from './NotFoundPage.module.css';
 const NotFoundPage = () => {
-  return <>NotFoundPage</>;
+  return (
+    <div className={css.errorBox}>
+      <IconContext.Provider value={{ size: '200px', color: '#101828' }}>
+        <PiSmileySadThin />
+      </IconContext.Provider>
+      <p className={css.errorMessage}>
+        <span className={css.errorSpan}>oops!</span>
+        Don`t worry, try again later
+      </p>
+      <Link to="/" className={css.btn}>
+        Back home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
